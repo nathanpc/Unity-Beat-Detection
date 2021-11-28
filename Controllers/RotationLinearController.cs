5750b7e1cb2637b459373490c364b0e29c15f33a
Nathan Campos <nathan@innoveworkshop.com>
2021-11-28 16:16:28 +0000

    Rotation transition smoothed

--- Controllers/RotationLinearController.cs
+++ Controllers/RotationLinearController.cs
@@ -15,27 +15,34 @@ public class RotationLinearController : LinearControllerBase {
 	public bool changeZRotation = true;
 	public float zChangeMultiplier = 1.0f;
 	private Quaternion originalRotation;
+	private Quaternion targetRotation;
 
 	protected override void Start() {
 		base.Start();
 
-		// Grab the initial position.
+		// Grab the initial rotation.
 		originalRotation = transform.localRotation;
+		targetRotation = transform.localRotation;
+	}
+
+	protected override void Update() {
+		base.Update();
+
+		// Smoothly transition to the target rotation.
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation,
+			Time.deltaTime * ChangeSpeed);
 	}
 
 	protected override void Change(float value) {
-		// Grab the original position.
-		Quaternion rotation = originalRotation;
+		// Grab the original rotation.
+		targetRotation = originalRotation;
 
 		// Set the values if needed.
 		if (changeXRotation)
-			rotation.x += value * xChangeMultiplier;
+			targetRotation.x += value * xChangeMultiplier;
 		if (changeYRotation)
-			rotation.y += value * yChangeMultiplier;
+			targetRotation.y += value * yChangeMultiplier;
 		if (changeZRotation)
-			rotation.z += value * zChangeMultiplier;
-
-		// Apply new position values.
-		transform.localRotation = rotation;
+			targetRotation.z += value * zChangeMultiplier;
 	}
 }
