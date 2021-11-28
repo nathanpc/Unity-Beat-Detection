c67fae13316f24b799774fbcb5d1fea25c13aaf5
Nathan Campos <nathan@innoveworkshop.com>
2021-11-28 12:08:26 +0000

    Changed the source formatting to Java style C#

--- Example.cs
+++ Example.cs
@@ -17,33 +17,29 @@
 using UnityEngine;
 using System;
 
-public class Example : MonoBehaviour
-{
+public class Example : MonoBehaviour {
 	public AudioProcessor processor;
 
-	void Start ()
-	{
-		processor.onBeat.AddListener (onOnbeatDetected);
-		processor.onSpectrum.AddListener (onSpectrum);
+	void Start() {
+		processor.onBeat.AddListener(onOnbeatDetected);
+		processor.onSpectrum.AddListener(onSpectrum);
 	}
 
 	//this event will be called every time a beat is detected.
 	//Change the threshold parameter in the inspector
 	//to adjust the sensitivity
-	void onOnbeatDetected ()
-	{
-		Debug.Log ("Beat!!!");
+	void onOnbeatDetected() {
+		Debug.Log("Beat!!!");
 	}
 
 	//This event will be called every frame while music is playing
-	void onSpectrum (float[] spectrum)
-	{
+	void onSpectrum(float[] spectrum) {
 		//The spectrum is logarithmically averaged
 		//to 12 bands
 
 		for (int i = 0; i < spectrum.Length; ++i) {
-			Vector3 start = new Vector3 (i, 0, 0);
-			Vector3 end = new Vector3 (i, spectrum [i], 0);
+			Vector3 start = new Vector3(i, 0, 0);
+			Vector3 end = new Vector3(i, spectrum[i], 0);
 
 			Debug.Log(i + " - " + spectrum[i]);
 			//Debug.DrawLine (start, end);
